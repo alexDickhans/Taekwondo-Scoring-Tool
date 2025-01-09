60ed94c5eb03bc5f4a263039b3999f120406090f
Alexander Dickhans <adickhans@gmail.com>
2025-01-09 11:52:56 -0700

    Basic functionality

--- lib/hierarchical_button.dart
+++ lib/hierarchical_button.dart
@@ -4,12 +4,16 @@ class HierarchicalButton extends StatefulWidget {
   final List<String> firstLevelButtonNames;
   final List<String> secondLevelButtonNames;
   final String showMenuText;
+  final List<String> sharedSelections;
+  final VoidCallback onSelectionChanged;
 
   const HierarchicalButton({
     Key? key,
     required this.firstLevelButtonNames,
     required this.secondLevelButtonNames,
     required this.showMenuText,
+    required this.sharedSelections,
+    required this.onSelectionChanged,
   }) : super(key: key);
 
   @override
@@ -17,32 +21,17 @@ class HierarchicalButton extends StatefulWidget {
 }
 
 class _HierarchicalButtonState extends State<HierarchicalButton> {
-  Map<String, Map<String, int>> _selectionCounts = {};
-  List<String> selections = [];
 
   @override
   void initState() {
     super.initState();
-    for (var name1 in widget.firstLevelButtonNames) {
-      _selectionCounts[name1] = {};
-      for (var name2 in widget.secondLevelButtonNames) {
-        _selectionCounts[name1]![name2] = 0;
-      }
-    }
-  }
-
-  @override
-  void didUpdateWidget(covariant HierarchicalButton oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.firstLevelButtonNames.length !=
-        widget.firstLevelButtonNames.length) {}
   }
 
   void _incrementCount(String name1, String name2, bool success) {
     setState(() {
-      _selectionCounts[name1]![name2] =
-          (_selectionCounts[name1]![name2] ?? 0) + 1;
-      selections.add(widget.showMenuText[0] + name1[0] + name2[0] + (success ? '1' : ''));
+      widget.sharedSelections.add(
+          widget.showMenuText[0] + name1[0] + name2[0] + (success ? '1' : ''));
+      widget.onSelectionChanged();
     });
   }
 
@@ -66,16 +55,12 @@ class _HierarchicalButtonState extends State<HierarchicalButton> {
                         onPressed: () => _incrementCount(name1, name2, false),
                         onLongPress: () => _incrementCount(name1, name2, true),
                         child:
-                            Text('$name2 (${_selectionCounts[name1]![name2]})'),
+                            Text('$name2'),
                       );
                     }).toList(),
               );
             }).toList(),
           ),
-        ),
-        Text(
-          'Selections: ${selections.join(" ")}',
-          style: TextStyle(fontSize: 18),
         ),
       ],
     );
--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -32,74 +31,95 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<String> _sharedSelections = [];
+  late List<HierarchicalButton> _hierarchicalButtons;
+
+  @override
+  void initState() {
+    super.initState();
+    _hierarchicalButtons = [
+      HierarchicalButton(
+        firstLevelButtonNames: ['Offense', 'Defense'],
+        secondLevelButtonNames: ['Body', 'Head'],
+        showMenuText: "Side",
+        sharedSelections: _sharedSelections,
+        onSelectionChanged: _updateSelections,
+      ),
+      HierarchicalButton(
+        firstLevelButtonNames: ['Offense', 'Defense'],
+        secondLevelButtonNames: ['Body', 'Head'],
+        showMenuText: "Round",
+        sharedSelections: _sharedSelections,
+        onSelectionChanged: _updateSelections,
+      ),
+      HierarchicalButton(
+        firstLevelButtonNames: ['Offense', 'Defense'],
+        secondLevelButtonNames: ['Body', 'Head'],
+        showMenuText: "Position",
+        sharedSelections: _sharedSelections,
+        onSelectionChanged: _updateSelections,
+      ),
+    ];
+  }
+
+  void _updateSelections() {
+    setState(() {});
+  }
 
   void _reset() {
     setState(() {
-      _counter++;
+      _sharedSelections.clear();
+    });
+  }
+
+  void _undo() {
+    setState(() {
+      if (_sharedSelections.isNotEmpty) {
+        _sharedSelections.removeLast();
+      }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            HierarchicalButton(
-              firstLevelButtonNames: ['Offense', 'Defense'],
-              secondLevelButtonNames: ['Body', 'Head'],
-              showMenuText: "Side",
-            ),
-            HierarchicalButton(
-              firstLevelButtonNames: ['Offense', 'Defense'],
-              secondLevelButtonNames: ['Body', 'Head'],
-              showMenuText: "Round",
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 2, // Adjust the number of columns as needed
+                children: _hierarchicalButtons,
+              ),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Selections: ${_sharedSelections.join(" ")}',
+              style: TextStyle(fontSize: 18),
             ),
           ],
         ),
       ),
-      floatingActionButton: FilledButton.icon(
-        onLongPress: _reset,
-        onPressed: () {},
-        label: const Text('Reset'),
-        icon: const Icon(Icons.refresh),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FilledButton.icon(
+              onPressed: () {},
+              onLongPress: _reset,
+              icon: const Icon(Icons.refresh),
+              label: const Text('Reset')),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            onPressed: _undo,
+            tooltip: 'Undo',
+            child: const Icon(Icons.undo),
+          ),
+        ],
+      ),
     );
   }
 }
